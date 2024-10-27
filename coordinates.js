e9b679e0712c193f183014567a88edd9ad57b9ec
Shimash Siraj <shimash.ca@gmail.com>
2024-10-27 17:38:10 -0400

    Files for the map application are added

---
+++ coordinates.js
@@ -0,0 +1,16 @@
+const coordinates = [
+    //Attractions
+    { "lat": 43.6559, "lng": -79.3804, "title": "Dundas Square", "category": "Attractions", "address": "1 Dundas St E, Toronto, ON M5B 2R8", "desc": "A bustling public square in downtown Toronto, often hosting events, concerts, and art displays. It's a vibrant gathering place surrounded by shops and restaurants." },
+    { "lat": 43.6401, "lng": -79.3758, "title": "Centre Island", "category": "Attractions", "address": "11 Queens Quay W, Toronto, ON M5J 2H3","desc": "One of the Toronto Islands, offering scenic views of the city skyline, beaches, parks, and attractions like Centreville Amusement Park. A popular spot for picnics and outdoor activities." },
+    { "lat": 43.6426, "lng": -79.3870, "title": "CN Tower", "category": "Attractions", "address": "290 Bremner Blvd, Toronto, ON M5V 3L9","desc": "One of the tallest free-standing structures in the world." },
+    { "lat": 43.6424, "lng": -79.3864, "title": "Ripley's Aquarium", "category": "Attractions", "address": "288 Bremner Blvd, Toronto, ON M5V 3L9","desc": "A popular family destination featuring thousands of marine species, interactive exhibits, and a mesmerizing underwater tunnel." },
+    //Subway Stations
+    { "lat": 43.6529, "lng": -79.3791, "title": "Queen Subway Station", "category": "Subway Stations" , "address": "8 Queen St W, Toronto, ON M5C 2X9","desc": "A busy subway station in downtown Toronto, located near Queen Street and many popular shopping and entertainment spots."},
+    { "lat": 43.6571, "lng": -79.3806, "title": "Dundas Subway Station", "category": "Subway Stations", "address": "Dundas Station, Toronto, ON","desc": "Located near Dundas Square and the Eaton Centre, this station is a key transit hub for travelers exploring the heart of the city." },
+    { "lat": 43.6497, "lng": -79.3778, "title": "King Subway Station", "category": "Subway Stations", "address": "3 King St E, Toronto, ON M5H 1A1","desc": "A subway station near Toronto’s Financial District, offering access to business centers, shopping, and dining." },
+    { "lat": 43.6461, "lng": -79.3804, "title": "Union Subway Station", "category": "Subway Stations", "address": "65 Front St W, Toronto, ON M5J 1E6","desc": "Toronto's main transit hub, connecting local subway lines with intercity trains and buses. It's close to major attractions like the CN Tower and Ripley's Aquarium." },
+    //Malls
+    { "lat": 43.6545, "lng": -79.3806, "title": "Eaton Centre", "category": "Malls", "address": "220 Yonge St, Toronto, ON M5B 2H1","desc": "Toronto’s largest shopping mall, featuring hundreds of stores, restaurants, and a stunning glass atrium." },
+    { "lat": 43.6558, "lng": -79.4357, "title": "Dufferin Mall", "category": "Malls", "address": "900 Dufferin St, Toronto, ON M6H 4A9","desc": "A popular shopping center located west of downtown Toronto, known for its wide range of stores, services, and family-friendly atmosphere." },
+    
+]
